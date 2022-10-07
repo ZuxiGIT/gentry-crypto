73683e583f01495785d2a6f07d4fa076c629d940
”uf00led” <uf00led@yandex.ru>
2022-10-07 23:27:38 +0300

    Adding a pre-finished versions of Vector & Matrix classes

--- include/math/Matrix.hpp
+++ include/math/Matrix.hpp
@@ -1,7 +1,9 @@
 #ifndef GENTRYCRYPTO_MATH_MATRIX_HPP
 #define GENTRYCRYPTO_MATH_MATRIX_HPP
 
-#include "Vector.hpp"
+#include <stdexcept>
+#include <utility>
+
 template <typename T>
 class Matrix
 {
@@ -13,7 +15,6 @@ class Matrix
     public:
         Matrix() = delete;    
         explicit Matrix(unsigned int row, unsigned int col);
-        explicit Matrix(const Vector<T>);
         ~Matrix();
         
         Matrix(const Matrix&);
@@ -32,27 +33,254 @@ class Matrix
         Matrix operator+() const;
         Matrix operator-() const;
 
-        T& operator()(unsigned int, unsigned int);
+        T& operator()(unsigned int, unsigned int) const;
 
         Matrix transpose();
+        
+        unsigned int getRowSize() const;
+        unsigned int getColSize() const;
 };
 
 template <typename T>
-Matrix<T> operator+(const Matrix<T>&, const Matrix<T>&);
+Matrix<T>::Matrix(unsigned int row, unsigned int col)
+: m_row(row),
+  m_col(col),
+  m_data(new T[m_col * m_row])
+{}
 
 template <typename T>
-Matrix<T> operator-(const Matrix<T>&, const Matrix<T>&);
+Matrix<T>::~Matrix()
+{
+    delete[] m_data; 
+}
 
 template <typename T>
-Matrix<T> operator*(const Matrix<T>&, const Matrix<T>&);
+Matrix<T>::Matrix(const Matrix<T>& rhs)
+: Matrix(rhs.m_row, rhs.m_col)
+{
+    std::copy(rhs.m_data, rhs.m_data + rhs.m_row * rhs.m_col, m_data);
+}
 
 template <typename T>
-Matrix<T> operator*(const Matrix<T>&, const double&);
+Matrix<T>& Matrix<T>::operator=(const Matrix<T>& rhs)
+{
+    if (this == &rhs)
+        return *this;
+
+    if (m_row != rhs.m_row || m_col != rhs.m_col)
+    {   
+        delete[] m_data;
+        m_row = rhs.m_row;
+        m_col = rhs.m_col;
+        m_data = new T[m_col * m_row];
+    }
+
+    std::copy(rhs.m_data, rhs.m_data + rhs.m_row * rhs.m_col, m_data);
+}
 
 template <typename T>
-Matrix<T> operator*(const double&, const Matrix<T>&);
+Matrix<T>::Matrix(Matrix<T>&& rhs)
+{
+    m_data = rhs.m_data;
+    m_col = rhs.m_col;
+    m_row = rhs.m_row;
+    rhs.m_data = nullptr;
+}
 
 template <typename T>
-Matrix<T> operator/(const Matrix<T>&, const double&);
+Matrix<T>& Matrix<T>::operator=(Matrix<T>&& rhs)
+{
+    delete[] m_data;
+    m_data = rhs.m_data;
+    m_col = rhs.m_col;
+    m_row = rhs.m_row;
+    rhs.m_data = nullptr;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator+=(const Matrix<T>& obj)
+{
+    if (m_row != obj.m_row|| m_col != obj.m_col)
+    {
+        throw std::domain_error("Error: Matrix spaces don't match. Please fix it and try again.");
+    }
+
+    for (int i = 0; i < m_row; i++)
+    {
+        for (int j = 0; j < m_col; j++)
+        {
+            m_data[i][j] += obj.m_data[i][j];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator-=(const Matrix<T>& obj)
+{
+    if (m_row != obj.m_row || m_col != obj.m_col)
+    {
+        throw std::domain_error("Error: Matrix spaces don't match. Please fix it and try again.");
+    }
+
+    for (int i = 0; i < m_row; i++)
+    {
+        for (int j = 0; j < m_col; j++)
+        {
+            m_data[i][j] -= obj.m_data[i][j];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator*=(const Matrix<T>& obj)
+{
+    if (m_row != obj.m_col)
+    {
+        throw std::domain_error("Error: Matrix spaces don't match. Please fix input and try again.");
+    }
+
+    Matrix<T> temp(obj.m_row, obj.m_col);
+    for (int i = 0; i < temp.m_row; i++)
+    {
+        for (int j = 0; j < temp.m_col; j++)
+        {
+            temp.m_data[i][j] = 0;
+            for (int k = 0; k < m_col; k++)
+            {
+                temp.m_data[i][j] += (m_data[i][k] * obj.m_data[k][j]);
+            }
+        }
+    }
+    return (*this = std::move(temp));
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator*=(const double& number)
+{
+    for (int i = 0; i < m_row; i++)
+    {
+        for (int j = 0; j < m_col; j++)
+        {
+            m_data[i][j] *= number;
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator/=(const double& number)
+{
+    for (int i = 0; i < m_row; i++)
+    {
+        for (int j = 0; j < m_col; j++)
+        {
+            m_data[i][j] /= number;
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator+() const
+{
+    return *this;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-() const
+{
+    Matrix<T> temp(m_row, m_col);
+    for (int i = 0; i < m_row; i++)
+    {
+        for (int j = 0; j < m_col; j++)
+        {
+            temp.m_data[i][j] = -m_data[i][j];
+        }
+    }
+    return temp;
+}
+
+template <typename T>
+Matrix<T> operator+(const Matrix<T>& l_obj, const Matrix<T>& r_obj)
+{
+    Matrix<T> temp(l_obj);
+    return (temp += r_obj);   
+}
+
+template <typename T>
+Matrix<T> operator-(const Matrix<T>& l_obj, const Matrix<T>& r_obj)
+{
+    Matrix<T> temp(l_obj);
+    return (temp -= r_obj);    
+}
+
+template <typename T>
+Matrix<T> operator*(const Matrix<T>& l_obj, const Matrix<T>& r_obj)
+{
+    Matrix<T> temp(l_obj);
+    return (temp *= r_obj);   
+}
+
+template <typename T>
+Matrix<T> operator*(const Matrix<T>& obj, const double& number)
+{
+    Matrix<T> temp(obj);
+    return (temp *= number);   
+}
+
+template <typename T>
+Matrix<T> operator*(const double& number, const Matrix<T>& obj)
+{
+    return obj * number;   
+}
+
+template <typename T>
+Matrix<T> operator/(const Matrix<T>& obj, const double& number)
+{
+    Matrix<T> temp(obj);
+    return (temp /= number);
+}
+
+template <typename T>
+T& Matrix<T>::operator()(unsigned int row, unsigned int col) const
+{
+    if (row == 0 || col == 0) 
+    {
+        throw std::domain_error("Error: References to indices start with 1. Please fix it and try again.");
+    }
+    else if (row > m_row || col > m_col) 
+    {
+        throw std::domain_error("Error: Going out of the matrix boundaries. Please fix it and try again.");
+    }
+    return *(m_data + m_row * (row - 1) + col - 1);
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::transpose()
+{
+    Matrix<T> temp(m_row, m_col);
+    for (int i = 0; i < m_row; i++)
+    {
+        for (int j = 0; j < m_col; j++)
+        {
+            temp.m_data[i][j] = m_data[j][i];
+        }
+    }
+    return temp;
+}
+
+template <typename T>
+unsigned int Matrix<T>::getColSize() const
+{
+    return m_col;
+}
+
+template <typename T>
+unsigned int Matrix<T>::getRowSize() const
+{
+    return m_row;
+}
 
 #endif // GENTRYCRYPTO_MATH_MATRIX_HPP
--- include/math/Vector.hpp
+++ include/math/Vector.hpp
@@ -1,6 +1,10 @@
 #ifndef GENTRYCRYPTO_MATH_VECTOR_HPP
 #define GENTRYCRYPTO_MATH_VECTOR_HPP
 
+#include "Matrix.hpp"
+#include <stdexcept>
+#include <utility>
+
 template <typename T>
 class Vector
 {
@@ -29,28 +33,245 @@ class Vector
         Vector operator+() const;
         Vector operator-() const;
 
-        T& operator()(unsigned int);
-
-        unsigned int getSize();
-        T* getData();
+        T& operator()(unsigned int) const; 
+        unsigned int getSize() const;
 };
 
 template <typename T>
-Vector<T> operator+(const Vector<T>&, const Vector<T>&);
+Vector<T>::Vector(unsigned int size)
+: v_size(size), 
+  v_data(new T[v_size])
+{}
 
 template <typename T>
-Vector<T> operator-(const Vector<T>&, const Vector<T>&);
+Vector<T>::~Vector()
+{
+    delete[] v_data; 
+}
 
 template <typename T>
-Vector<T> operator*(const Vector<T>&, const Vector<T>&);
+Vector<T>::Vector(const Vector<T>& rhs)
+: Vector(rhs.v_size)
+{
+    std::copy(rhs.v_data, rhs.v_data + rhs.v_size, v_data);
+}
 
 template <typename T>
-Vector<T> operator*(const Vector<T>&, const double&);
+Vector<T>& Vector<T>::operator=(const Vector<T>& rhs)
+{
+    if (this == &rhs)
+        return *this;
+
+    if (v_size != rhs.v_size)
+    {   
+        delete[] v_data;
+        v_size = rhs.v_size;
+        v_data = new T[v_size];
+    }
+
+    std::copy(rhs.v_data, rhs.v_data + rhs.v_size, v_data);
+    return *this;
+}
 
 template <typename T>
-Vector<T> operator*(const double&, const Vector<T>&);
+Vector<T>::Vector(Vector<T>&& rhs)
+{
+    v_data = rhs.v_data;
+    v_size = rhs.v_size;
+    rhs.v_data = nullptr;
+}
 
 template <typename T>
-Vector<T> operator/(const Vector<T>&, const double&);
+Vector<T>& Vector<T>::operator=(Vector<T>&& rhs)
+{
+    delete[] v_data;
+    v_data = rhs.v_data;
+    v_size = rhs.v_size;
+    rhs.v_data = nullptr;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator+=(const Vector<T>& obj)
+{
+    if (v_size != obj.v_size)
+    {
+        throw std::domain_error("Error: Vector spaces don't match. Please fix it and try again.");
+    }
+
+    for (int i = 0; i < v_size; i++)
+    {
+        v_data[i] += obj.v_data[i];
+    }
+    return *this;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator-=(const Vector<T>& obj)
+{
+    if (v_size != obj.v_size)
+    {
+        throw std::domain_error("Error: Vector spaces don't match. Please fix it and try again.");
+    }
+
+    for (int i = 0; i < v_size; i++)
+    {
+        v_data[i] -= obj.v_data[i];
+    }
+    return *this;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator*=(const Vector<T>& obj)
+{
+    if (v_size != obj.v_size)
+    {
+        throw std::domain_error("Error: Vector spaces don't match. Please fix input and try again.");
+    }
+
+    Vector<T> temp(obj.v_size);
+    for (int i = 0; i < v_size; i++)
+    {
+        temp.v_data[i] = v_data[i] * obj.v_data[i];
+    }
+    return (*this = std::move(temp));
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator*=(const double& number)
+{
+    for (int i = 0; i < v_size; i++)
+    {
+        v_data[i] *= number;
+    }
+    return *this;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator/=(const double& number)
+{
+    for (int i = 0; i < v_size; i++)
+    {
+        v_data[i] /= number;
+    }
+    return *this;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator+() const
+{
+    return *this;
+}
+
+template <typename T>
+Vector<T> Vector<T>::operator-() const
+{
+    Vector<T> temp(v_size);
+    for (int i = 0; i < v_size; i++)
+    {
+        temp[i] -= v_data[i];
+    }
+    return temp;
+}
+
+template <typename T>
+Vector<T> operator+(const Vector<T>& l_obj, const Vector<T>& r_obj)
+{
+    Vector<T> temp(l_obj);
+    return (temp += r_obj);   
+}
+
+template <typename T>
+Vector<T> operator-(const Vector<T>& l_obj, const Vector<T>& r_obj)
+{
+    Vector<T> temp(l_obj);
+    return (temp -= r_obj);    
+}
+
+template <typename T>
+Vector<T> operator*(const Vector<T>& l_obj, const Vector<T>& r_obj)
+{
+    Vector<T> temp(l_obj);
+    return (temp *= r_obj);   
+}
+
+template <typename T>
+Vector<T> operator*(const Vector<T>& l_obj, const Matrix<T>& r_obj)
+{
+    if (l_obj.getSize() != r_obj.getColSize())
+    {
+        throw std::domain_error("Error: Spaces between Vector and Matrix don't match. Please fix input and try again.");
+    }
+
+    Vector<T> temp(l_obj.getSize());
+    for (unsigned int i = 1; i < r_obj.getRowSize() + 1; i++)
+    {
+        temp(i) = 0;
+        for (unsigned int j = 1; j < l_obj.getSize() + 1; j++)
+        {
+            temp(i) += l_obj(j) * r_obj(j, i);
+        }
+    }
+    return temp;
+}
+
+template <typename T>
+Vector<T> operator*(const Matrix<T>& l_obj, const Vector<T>& r_obj)
+{
+    if (l_obj.getRowSize() != r_obj.getSize())
+    {
+        throw std::domain_error("Error: Spaces between Matrix and Vector don't match. Please fix input and try again.");
+    }
+
+    Vector<T> temp(r_obj.getSize());
+    for (unsigned int i = 1; i < l_obj.getColSize() + 1; i++)
+    {
+        temp(i) = 0;
+        for(unsigned int j = 1; j < r_obj.getSize() + 1; j++)
+        {
+            temp(i) += l_obj(i, j) * r_obj(j);
+        }
+    }
+    return temp; 
+}
+
+template <typename T>
+Vector<T> operator*(const Vector<T>& obj, const double& number)
+{
+    Vector<T> temp(obj);
+    return (temp *= number);   
+}
+
+template <typename T>
+Vector<T> operator*(const double& number, const Vector<T>& obj)
+{
+    return obj * number;   
+}
+
+template <typename T>
+Vector<T> operator/(const Vector<T>& obj, const double& number)
+{
+    Vector<T> temp(obj);
+    return (temp /= number);
+}
+
+template <typename T>
+T& Vector<T>::operator()(unsigned int elem) const
+{
+    if (v_size == 0) 
+    {
+        throw std::domain_error("Error: References to indices start with 1. Please fix it and try again.");
+    }
+    else if (elem > v_size) 
+    {
+        throw std::domain_error("Error: Going out of the Vector boundaries. Please fix it and try again.");
+    }
+    return v_data[elem - 1];
+}
+
+template <typename T>
+unsigned int Vector<T>::getSize() const
+{
+    return v_size;
+}
 
 #endif // GENTRYCRYPTO_MATH_VECTOR_HPP
